d4bda03b09f590689c0b59a7051030da691e9111
Nadi Adem <NAdem@spartaglobal.com>
2019-01-11 11:46:12 +0000

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+
+console.log("hi");
+
+var number = 4;
+var otherNum =7;
+var comment = "wooo";
+var sum = number + otherNum;
+console.log("The sum of ",number,"and",otherNum, "=",sum);
+
+if (number < otherNum)
+{
+    console.log("number is larger than otherNum");
+    while(number < otherNum)
+    {
+        
+    }
+}
